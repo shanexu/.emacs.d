5989a0542cc4ab7770a2f3fab11ab3a3ba43ce18
Shane Xu <xusheng0711@gmail.com>
2023-05-21 10:04:52 +0800

    format

--- init.el
+++ init.el
@@ -1,115 +1,98 @@
-(message "hello world")
+  (message "hello world")
 
-(setq straight-use-package-by-default t)
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 6))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-	(url-retrieve-synchronously
-	 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-	 'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+  (setq straight-use-package-by-default t)
+  (defvar bootstrap-version)
+  (let ((bootstrap-file
+         (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+        (bootstrap-version 6))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+          (url-retrieve-synchronously
+           "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+           'silent 'inhibit-cookies)
+        (goto-char (point-max))
+        (eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage))
 
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
+  (menu-bar-mode -1)
+  (scroll-bar-mode -1)
+  (tool-bar-mode -1)
 
-(setq inhibit-startup-message t)
+  (setq inhibit-startup-message t)
 
-(use-package standard-themes)
+  (use-package standard-themes)
 
-(use-package kaolin-themes
-  :config
-  (load-theme 'kaolin-dark t)
-  (kaolin-treemacs-theme))
+  (use-package kaolin-themes
+    :config
+    (load-theme 'kaolin-dark t))
 
-(defun load-dark-theme ()
-  (load-theme 'kaolin-dark t))
+  (defun load-dark-theme ()
+    (load-theme 'kaolin-dark t))
 
-(defun load-light-theme ()
-  (load-theme 'kaolin-light t))
+  (defun load-light-theme ()
+    (load-theme 'kaolin-light t))
 
-(use-package auto-dark
-  :straight (auto-dark :type git :host github :repo "shanexu/auto-dark-emacs")
-  :config
-  (setq auto-dark-dark-mode-hook 'load-dark-theme)
-  (setq auto-dark-light-mode-hook 'load-light-theme)
-  (auto-dark-mode t))
+  (use-package auto-dark
+    :straight (auto-dark :type git :host github :repo "shanexu/auto-dark-emacs")
+    :config
+    (setq auto-dark-dark-mode-hook 'load-dark-theme)
+    (setq auto-dark-light-mode-hook 'load-light-theme)
+    (auto-dark-mode t))
 
-(use-package cnfonts
-  :config
-  (cnfonts-mode 1))
+  (use-package cnfonts
+    :config
+    (cnfonts-mode 1))
 
-(use-package org
-  :hook
-  (org-mode . org-indent-mode))
+  (use-package org
+    :hook
+    (org-mode . org-indent-mode))
 
-(use-package org-superstar
-  :hook
-  (org-mode . org-superstar-mode))
+  (use-package org-superstar
+    :hook
+    (org-mode . org-superstar-mode))
 
-(use-package vertico
-  :init
-  (vertico-mode)
+  (use-package vertico
+    :straight (vertico :type git :host github :repo "minad/vertico" :files ("*.el" "extensions/*.el"))
+    :init
+    (vertico-mode)
+    (defun crm-indicator (args)
+      (cons (format "[CRM%s] %s"
+                    (replace-regexp-in-string
+                     "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                     crm-separator)
+                    (car args))
+            (cdr args)))
+    (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+    :config
+    (setq vertico-resize nil
+          vertico-count 17
+          vertico-cycle t)
+    (setq-default completion-in-region-function
+                  (lambda (&rest args)
+                    (apply (if vertico-mode
+                               #'consult-completion-in-region
+                             #'completion--in-region)
+                           args)))
+    :bind (:map vertico-map ("DEL" . vertico-directory-delete-char))
+    :hook
+    (minibuffer-setup . vertico-repeat-save)
+    (rfn-eshadow-update-overlay . vertico-directory-tidy)
+    )
 
-  ;; Different scroll margin
-  ;; (setq vertico-scroll-margin 0)
+  ;; Persist history over Emacs restarts. Vertico sorts by history position.
+  (use-package savehist
+    :init
+    (savehist-mode))
 
-  ;; Show more candidates
-  ;; (setq vertico-count 20)
+  ;; Optionally use the `orderless' completion style.
+  (use-package orderless
+    :init
+    ;; Configure a custom style dispatcher (see the Consult wiki)
+    ;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
+    ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+    (setq completion-styles '(orderless basic)
+          completion-category-defaults nil
+          completion-category-overrides '((file (styles partial-completion)))))
 
-  ;; Grow and shrink the Vertico minibuffer
-  ;; (setq vertico-resize t)
-
-  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
-  ;; (setq vertico-cycle t)
-  )
-
-;; Persist history over Emacs restarts. Vertico sorts by history position.
-(use-package savehist
-  :init
-  (savehist-mode))
-
-;; A few more useful configurations...
-(use-package emacs
-  :init
-  ;; Add prompt indicator to `completing-read-multiple'.
-  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
-  (defun crm-indicator (args)
-    (cons (format "[CRM%s] %s"
-                  (replace-regexp-in-string
-                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
-                   crm-separator)
-                  (car args))
-          (cdr args)))
-  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
-
-  ;; Do not allow the cursor in the minibuffer prompt
-  (setq minibuffer-prompt-properties
-        '(read-only t cursor-intangible t face minibuffer-prompt))
-  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
-
-  ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
-  ;; Vertico commands are hidden in normal buffers.
-  ;; (setq read-extended-command-predicate
-  ;;       #'command-completion-default-include-p)
-
-  ;; Enable recursive minibuffers
-  (setq enable-recursive-minibuffers t))
-
-;; Optionally use the `orderless' completion style.
-(use-package orderless
-  :init
-  ;; Configure a custom style dispatcher (see the Consult wiki)
-  ;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
-  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
-  (setq completion-styles '(orderless basic)
-        completion-category-defaults nil
-        completion-category-overrides '((file (styles partial-completion)))))
-
-(use-package vterm
-  :ensure t)
+  (use-package vterm
+    :ensure t)
